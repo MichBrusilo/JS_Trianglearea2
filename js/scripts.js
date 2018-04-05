d1cd27978eea550fda32aafbe35bb3370e55c17c
Michał Brusiło <mibru@interia.pl>
2018-04-05 09:06:18 +0200

    Add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//zadanie 9_1
+function getTriangleArea(a, h) {
+  if (a <= 0 || h <= 0) {
+    return ("Nieprawidlowe dane")
+  }
+  else {
+    return a*h/2
+  }
+}
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(10, 15);
+console.log(triangle1Area);
+
+var triangle2Area = getTriangleArea(8, 32);
+console.log(triangle2Area);
+
+var triangle3Area = getTriangleArea(25, 40);
+console.log(triangle3Area);
